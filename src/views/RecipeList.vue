3a4d15cef2e1b29439a32e34416b1d31fbf9b284
Jarek Grabarczyk <jaroslaw.grabarczyk@projectic.pl>
2019-02-27 00:32:58 +0100

    upgrade search function

--- src/views/RecipeList.vue
+++ src/views/RecipeList.vue
@@ -33,14 +33,40 @@ export default {
       this.search = data;
     });
   },
-  computed: {
-    filterRecipes() {
-      return this.recipes.filter(recipe => {
-        return recipe.title.toLowerCase().includes(this.search.toLowerCase());
+  methods: {
+    filterList(q, list) {
+      function escapeRegExp(s) {
+        return s.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+      }
+      const words = q
+        .split(/\s+/g)
+        .map(s => s.trim())
+        .filter(s => !!s);
+      const hasTrailingSpace = q.endsWith(" ");
+      const searchRegex = new RegExp(
+        words
+          .map((word, i) => {
+            if (i + 1 === words.length && !hasTrailingSpace) {
+              // The last word - ok with the word being "startswith"-like
+              return `(?=.*\\b${escapeRegExp(word)})`;
+            } else {
+              // Not the last word - expect the whole word exactly
+              return `(?=.*\\b${escapeRegExp(word)}\\b)`;
+            }
+          })
+          .join("") + ".+",
+        "gi"
+      );
+      return list.filter(item => {
+        return searchRegex.test(item.title);
       });
     }
   },
-  methods: {}
+  computed: {
+    filterRecipes() {
+      return this.filterList(this.search, this.recipes);
+    }
+  }
 };
 </script>
 
