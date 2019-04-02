eed3fc9ea2aec69cbfa64269ccab397ab1b98402
Jarek Grabarczyk <jaroslaw.grabarczyk@projectic.pl>
2019-04-02 19:02:57 +0200

    clean up

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -23,14 +23,6 @@ export default {
   methods: {
     handleScroll(event) {
       this.isScrolledTop = window.scrollY == 0 ? true : false;
-      console.log(this.isScrolledTop);
-      // if (isScrolledTop && this.isScrolledTop ) {
-      //   this.isScrolledTop = false;
-      // }
-
-      // if (!isScrolledTop && !this.isScrolledTop) {
-      //   this.isScrolledTop = true;
-      // }
     }
   },
   created() {
